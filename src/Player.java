d51f647fc8ddb70a3c629d13fa02c8e9c32869f1
Richard Hoggan <richardhoggan@Richards-MacBook.local>
2016-07-14 19:31:55 -0700

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,69 @@
+class Player 
+{
+	//Class Data Members
+	private String firstName;
+	private String lastName;
+	private int gameScore;
+	private boolean playersTurnFlag;
+	private boolean lostTurnFlag;
+	private boolean bankruptFlag;
+	private int extraTurnsCounter;
+	private int correctQuestionsCounter;
+	private int missedQuestionsCounter;
+	
+	//Class Constructors
+	public Player()
+	{
+		this.firstName = "";
+		this.lastName = "";
+		this.gameScore = 0;
+		this.playersTurnFlag = false;
+		this.lostTurnFlag = false;
+		this.bankruptFlag = false;
+		this.extraTurnsCounter = 0;
+		this.correctQuestionsCounter = 0;
+		this.missedQuestionsCounter = 0;
+	}
+	
+	public Player(String firstName, String lastName, int gameScore, 
+		boolean playersTurnFlag, boolean lostTurnFlag, boolean bankruptFlag,
+		int extraTurnsCounter, int correctQuestionsCounter, int missedQuestionCounter)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gameScore = gameScore;
+		this.playersTurnFlag = playersTurnFlag;
+		this.lostTurnFlag = lostTurnFlag;
+		this.bankruptFlag = bankruptFlag;
+		this.extraTurnsCounter = extraTurnsCounter;
+		this.missedQuestionsCounter = missedQuestionsCounter;
+	}
+	
+	//Getters and Setters
+	public String getFirstName(){return this.firstName;}
+	public String getLastName(){return this.lastName;}
+	public int getGameScore(){return this.gameScore;}
+	public boolean isPlayersTurn(){return this.playersTurnFlag;}
+	public boolean playerLostTurn(){return this.lostTurnFlag;}
+	public boolean isBankrupt(){return this.bankruptFlag;}
+	public int getExtraTurnsCounter(){return this.extraTurnsCounter;}
+	public int getCorrectQuestionsCounter(){return this.correctQuestionsCounter;}
+	public int getMissedQuestionsCounter(){return this.missedQuestionsCounter;}
+	
+	public void setFirstName(String firstName){this.firstName = firstName;}
+	public void setLastName(String lastName){this.lastName = lastName;}
+	public void setGameScore(int gameScore){this.gameScore = gameScore;}
+	public void setPlayersTurnFlag(boolean playersTurnFlag){this.playersTurnFlag = playersTurnFlag;}
+	public void setLostTurnFlag(boolean lostTurnFlag){this.lostTurnFlag = lostTurnFlag;}
+	public void setBankruptFlag(boolean bankruptFlag){this.bankruptFlag = bankruptFlag;}
+	public void setExtraTurnsCounter(int extraTurnsCounter){this.extraTurnsCounter = extraTurnsCounter;}
+	public void setCorrectQuestionsCounter(int correctQuestionsCounter){this.correctQuestionsCounter = correctQuestionsCounter;}
+	public void setMissedQuestionsCounter(int missedQuestionsCounter){this.missedQuestionsCounter = missedQuestionsCounter;}
+	
+	//Action Methods
+	public boolean makePlayerBankrupt(){this.gameScore = 0;return true;}
+	public void incrementPlayerScore(int scoreIncrease){this.gameScore += scoreIncrease;}
+	public void decrementPlayerScore(int scoreDecrease){this.gameScore -= scoreDecrease;}
+	public void incrementCorrectQuestionsCounter(){this.correctQuestionsCounter += 1;}
+	public void incrementMissedQuestionsCounter(){this.missedQuestionsCounter += 1;}
+}
