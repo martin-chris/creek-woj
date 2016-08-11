941484c8fb7a1d060bf2dbc2d66e0c2d75dbfa90
katxleen <ktam8@jhu.edu>
2016-08-11 11:36:55 -0400

    Change ScoreBoard label to Score Board Button

--- src/main/woj/ui/GameplayPage.java
+++ src/main/woj/ui/GameplayPage.java
@@ -18,12 +18,23 @@ public class GameplayPage extends JPanel{
 	private GameFrame gameFrame;
 	private GameplayContainer gameplayContainer;
 	private ActionController controller;
+	private JLabel emptyLabel;
+	private JButton scoreBoardButton;
 	
 	public GameplayPage(GameFrame gameFrame, ActionController controller){
 		this.gameFrame = gameFrame;
 		this.controller = controller;
 		setup();
 		initComponents();
+		initListeners();
+	}
+	
+	private void initListeners() {
+		scoreBoardButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {	
+				ScoreBoard.showScoreBoard(gameFrame.gameModel);
+			}
+        });
 	}
 
 	private void setup() {
@@ -33,9 +44,11 @@ public class GameplayPage extends JPanel{
 
 	private void initComponents() {
 		gameplayContainer = new GameplayContainer(controller);
-		scoreBoard = new ScoreBoard();
+		//scoreBoard = new ScoreBoard();
+		scoreBoardButton = new JButton("Score");
+		emptyLabel = new JLabel("");
 		actionIndicator = new ActionIndicator();
-		scoreBoard = new ScoreBoard();
+		//scoreBoard = new ScoreBoard();
 		this.add(gameplayContainer, BorderLayout.CENTER);
 		this.add(bottomPanel(), BorderLayout.SOUTH);
 	}
@@ -54,8 +67,10 @@ public class GameplayPage extends JPanel{
 	
 	private JPanel bottomPanel(){
 		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new GridLayout(1,2));
-		bottomPanel.add(scoreBoard);
+		bottomPanel.setLayout(new GridLayout(1,3));
+		bottomPanel.add(scoreBoardButton);
+		bottomPanel.add(emptyLabel);
+		//bottomPanel.add(scoreBoard);
 		bottomPanel.add(actionIndicator);
 		return bottomPanel;
 	}
